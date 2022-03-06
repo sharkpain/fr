ec0708c2033e4607ecf92451e024839ac8e8ebfa
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-03-06 17:55:28 +0200

    Update fr.js

--- src/fr.js
+++ src/fr.js
@@ -30,7 +30,45 @@ function onMessage(message) {
 }
 module.exports = {
     execute: function (message, args, util) {
-       
+        sharkdb.foc(message.author.id, user => {
+            if(user.fr.banned) return message.channel.send("you have been banned from frs you fart");
+            switch(args[0]) {
+                case "list":
+                    sharkdb.frCol().find({creator: message.author.id}, (err, frs) => {
+                        if (err) {
+                            apis["core-error"].api.error(err);
+                            return message.channel.send("it broke lol");
+                        }
+                        if(frs.length < 1) return message.channel.send("you have no frs");
+                        let embed = new MessageEmbed()
+                        .setTitle("Frs")
+                        .setDescription(frs.map(fr => `${fr.trigger} - used ${fr.timesTriggered} times`).join("\n"))
+                        message.channel.send({embeds: [embed]});
+                    })
+                    break;
+                case "add":
+                    break;
+                case "remove":
+                    break;
+                case "shutup":
+                    break;
+                case "global":
+                    sperms.permittedTo("frglobalmod", message.author.id, message.guild.id, (perm, user) => {
+                        if(!perm) return message.channel.send("you dont have that permission, missing frglobalmod");
+                        message.channel.send("todo: finish fr global")
+                    })
+                    break;
+                case "ban":
+                    sperms.permittedTo("frmod", message.author.id, message.guild.id, (perm, user) => {
+                        if(!perm) return message.channel.send("you dont have that permission, missing frmod");
+                        message.channel.send("todo: finish fr ban")
+                    })
+                    break;
+                default:
+                    message.channel.send("fr [list/add/remove/shutup (global/ban for frglobalmod and frmod)]");
+                    break;
+            }
+        })
     },
 	registerEventHandlers: function (cb) {
         cb("messageCreate", onMessage);
