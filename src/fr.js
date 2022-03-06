4b8d891313fc3337fa293fb2cc115bb3e8d4b324
Skelly <47392011+vtheskeleton@users.noreply.github.com>
2022-03-06 18:37:47 +0200

    Update fr.js

--- src/fr.js
+++ src/fr.js
@@ -8,69 +8,111 @@ let errored = false
 
 function onMessage(message) {
 	if (errored) return;
-    if(!frCache) {
-        sharkdb.getFrs(frs => {
-            if(frs == "ERR") errored = true;
-            frCache = frs;
-            return onMessage(message);
-        })
-    } else {
-        let fr = frCache.find(fr => fr.trigger == message.content)
-        if(!fr) return
-        if(fr.creator != message.author.id && !fr.global) return
-        if(fr.global && fr.forcedUnglobal && fr.creator != message.author.id) return
-        sharkdb.foc(message.author.id, user => {
-            if(!user) return
-            if(user.frDisabled) return
-            if(user.banned) return
-            message.channel.send(fr.response);
-            sharkdb.incrementFr(fr._id);
-        })
-    }
+	if(!frCache) {
+		sharkdb.getFrs(frs => {
+			if(frs == "ERR") errored = true;
+			frCache = frs;
+			return onMessage(message);
+		})
+	} else {
+		let fr = frCache.find(fr => fr.trigger == message.content)
+		if(!fr) return
+		if(fr.creator != message.author.id && !fr.global) return
+		if(fr.global && fr.forcedUnglobal && fr.creator != message.author.id) return
+		sharkdb.foc(message.author.id, user => {
+			if(!user) return
+			if(user.frDisabled) return
+			if(user.banned) return
+			message.channel.send(fr.response);
+			sharkdb.incrementFr(fr._id);
+		})
+	}
 }
 module.exports = {
-    execute: function (message, args, util) {
-        sharkdb.foc(message.author.id, user => {
-            if(user.fr.banned) return message.channel.send("you have been banned from frs you fart");
-            switch(args[0]) {
-                case "list":
-                    sharkdb.frCol().find({creator: message.author.id}, (err, frs) => {
-                        if (err) {
-                            apis["core-error"].api.error(err);
-                            return message.channel.send("it broke lol");
-                        }
-                        if(frs.length < 1) return message.channel.send("you have no frs");
-                        let embed = new MessageEmbed()
-                        .setTitle("Frs")
-                        .setDescription(frs.map(fr => `${fr.trigger} - used ${fr.timesTriggered} times`).join("\n"))
-                        message.channel.send({embeds: [embed]});
-                    })
-                    break;
-                case "add":
-                    break;
-                case "remove":
-                    break;
-                case "shutup":
-                    break;
-                case "global":
-                    sperms.permittedTo("frglobalmod", message.author.id, message.guild.id, (perm, user) => {
-                        if(!perm) return message.channel.send("you dont have that permission, missing frglobalmod");
-                        message.channel.send("todo: finish fr global")
-                    })
-                    break;
-                case "ban":
-                    sperms.permittedTo("frmod", message.author.id, message.guild.id, (perm, user) => {
-                        if(!perm) return message.channel.send("you dont have that permission, missing frmod");
-                        message.channel.send("todo: finish fr ban")
-                    })
-                    break;
-                default:
-                    message.channel.send("fr [list/add/remove/shutup (global/ban for frglobalmod and frmod)]");
-                    break;
-            }
-        })
-    },
+	execute: function (message, args, util) {
+		sharkdb.foc(message.author.id, user => {
+			if(user.fr.banned) return message.channel.send("you have been banned from frs you fart");
+			switch(args[0]) {
+				case "list":
+					sharkdb.frCol().find({creator: message.author.id}, (err, frs) => {
+						if (err) {
+							apis["core-error"].api.error(err);
+							return message.channel.send("it broke lol");
+						}
+						if(frs.length < 1) return message.channel.send("you have no frs");
+						let embed = new MessageEmbed()
+						.setTitle("Frs")
+						.setDescription(frs.map(fr => `${fr.trigger} - used ${fr.timesTriggered} times`).join("\n"))
+						message.channel.send({embeds: [embed]});
+					})
+					break;
+				case "add":
+					message.channel.send("fr setup: part 1\n**type the words that will trigger thsi fr**\ntype cancel to cancel");
+					let filter = m => m.author.id == message.author.id;
+					let collector = message.channel.createMessageCollector({ filter, time: 20000 });
+					collector.on('collect', function (m) {
+						if (m.content.toLowerCase() == "cancel") {
+							message.channel.send("cancelled");
+							return this.stop()
+						}
+						this.stop("response")
+						let trigger = m.content;
+						message.channel.send("fr setup: part 2\n**type what should be said when fr is triggered**\n(u can also attach a file)\ntype cancel to cancel");
+						let filter = m => m.author.id == message.author.id;
+						let collector = message.channel.createMessageCollector({ filter, time: 20000 });
+						collector.on('collect', function (m) {
+							if (m.content.toLowerCase() == "cancel") {
+								message.channel.send("cancelled");
+								return this.stop()
+							}
+							this.stop("response")
+							let content = m.content;
+							if (m.attachments.length > 0) {
+								m.attachment.forEach(a => content += `\n${a.proxyURL}`)
+							}
+							sharkdb.addFr(trigger, content, message.author.id, fr => {
+								if(fr == "ERR_BANNED") return message.channel.send("you have been banned from frs you fart (this message should never be seen, so congrats)");
+								if(fr == "ERR_DBFAIL") return message.channel.send("something went wrong, try again later");
+								message.channel.send(`created fr successfully!\ntrigger words: ${trigger}\nmessage: ${content}`);
+								frCache.push(fr);
+								
+							})
+						});
+						collector.on('end', (collected, reason) => {
+							if(reason == "time") {
+								message.channel.send("i sent you my paws please respond")
+							}
+						});
+					});
+					collector.on('end', (collected, reason) => {
+						if(reason == "time") {
+							message.channel.send("i sent you my paws please respond")
+						}
+					});
+					break;
+				case "remove":
+					break;
+				case "shutup":
+					break;
+				case "global":
+					sperms.permittedTo("frglobalmod", message.author.id, message.guild.id, (perm, user) => {
+						if(!perm) return message.channel.send("you dont have that permission, missing frglobalmod");
+						message.channel.send("todo: finish fr global")
+					})
+					break;
+				case "ban":
+					sperms.permittedTo("frmod", message.author.id, message.guild.id, (perm, user) => {
+						if(!perm) return message.channel.send("you dont have that permission, missing frmod");
+						message.channel.send("todo: finish fr ban")
+					})
+					break;
+				default:
+					message.channel.send("fr [list/add/remove/shutup (global/ban for frglobalmod and frmod)]");
+					break;
+			}
+		})
+	},
 	registerEventHandlers: function (cb) {
-        cb("messageCreate", onMessage);
-    }
+		cb("messageCreate", onMessage);
+	}
 }
